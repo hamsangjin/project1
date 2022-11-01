9d834bdbb9b0e7100a2a3a63e9d89306684f9a7e
hamsangjin <rhkstlash@naver.com>
2022-11-01 12:45:21 +0900

    돔을 이용해 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+// login.ejs 파일과 연결되어 있음
+// DOM : Document Object Model
+
+// 선택자(login.ejs에 있는 태그)를 통해서 html 값을 가져올 수 있다.
+
+// # : 태그에 id에 부여되어있는 id, psword를 선택해라
+const id = document.querySelector("#id"); 
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+  const req = {
+    id: id.value,
+    psword: psword.value
+  };
+  console.log(req);
+}
+
