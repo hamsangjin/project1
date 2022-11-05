31639db5fcb8cebb1e438b7f85a4eaa51c4b4581
hamsangjin <rhkstlash@naver.com>
2022-11-05 15:21:39 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -32,6 +32,16 @@ function login(){
     // Response는 데이터가 모두 받아진 상태가 아니다, json으로 Response 스트림을 가져와 완료될 때까지 읽는다
     // 다 읽은 body 텍스트를 Promise 형태로 반환한다.
   }).then((res) => res.json())
-    .then((res) => console.log(res));  
+    .then((res) => {
+      if (res.success){
+        location.href = "/";
+      } else{
+        alert(res.msg);
+      }
+    })
+    // 로그인 에러 제거
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    });  
 }
 
